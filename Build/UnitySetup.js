9c174ea6f983be1c68700f9f9666df0a6cafbb59
Helmy5d <mohamed.helmy@5d-vr.com>
2023-10-05 19:26:02 +0300

    .

--- Build/UnitySetup.js
+++ Build/UnitySetup.js
@@ -18,40 +18,6 @@ var config = {
     productVersion: "0.1",
 };
 
-// By default Unity keeps WebGL canvas render target size matched with
-// the DOM size of the canvas element (scaled by window.devicePixelRatio)
-// Set this to false if you want to decouple this synchronization from
-// happening inside the engine, and you would instead like to size up
-// the canvas DOM size and WebGL render target sizes yourself.
-// config.matchWebGLToCanvasSize = false;
-
-if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
-    // Mobile device style: fill the whole browser client area with the game canvas:
-    var meta = document.createElement('meta');
-    meta.name = 'viewport';
-    meta.content = 'width=device-width, height=device-height, initial-scale=1.0, user-scalable=no, shrink-to-fit=yes';
-    document.getElementsByTagName('head')[0].appendChild(meta);
-
-    var canvas = document.querySelector("#unity-canvas");
-    canvas.style.width = "100%";
-    canvas.style.height = "100%";
-    canvas.style.position = "fixed";
-
-    document.body.style.textAlign = "left";
-}
-
-function onResize() {
-    console.log("resize");
-    var canvas = document.querySelector("#unity-canvas");
-    canvas.height = window.innerHeight;
-    canvas.width = window.innerWidth;
-    console.log("set size: ", canvas.width, canvas.height);
-}
-
-window.addEventListener("resize", function () {
-    onResize();
-});
-onResize();
 
 var loadingBar = document.querySelector("#unity-loading-bar");
 var progressBarFull = document.querySelector("#unity-progress-bar-full");
